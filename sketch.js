775bbca93ca9fb3f718d54d820633ea1df666119
sindhoora93 <75593694+sindhoora93@users.noreply.github.com>
2020-12-22 09:24:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect,movingRect;
+
+function setup() {
+ createCanvas(1200,800);
+ fixedRect=createSprite(200,200,50,80);
+ fixedRect.shapeColor="green";
+  movingRect=createSprite(400, 200, 50, 50);
+  movingRect.shapeColor="green";
+}
+
+function draw() {
+  background("yellow"); 
+
+  console.log(movingRect.x-fixedRect.x);
+
+  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2&&
+    fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2&&
+    movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.width/2&&
+    fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.width/2){
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+
+  else{
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
+  }
+
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  
+  drawSprites();
+}
